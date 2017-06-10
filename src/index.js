f95267b29e531edd36fe13120fd7160540fd1972
Fahad Hossain <evilinfinite7@gmail.com>
2017-06-11 02:32:56 +0600

    roll back router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'mobx-react'
 import { AppContainer } from 'react-hot-loader'
-import { HashRouter as Router, Route } from 'react-router-dom'
 
 import App from './App'
 import DataStore from './DataStore'
@@ -12,31 +11,21 @@ import 'bootswatch/cosmo/bootstrap.css'
 import './index.css'
 
 const render = Component => {
-  ReactDOM.render(<Component />, document.getElementById('root'))
+  ReactDOM.render(
+    <AppContainer>
+      <Provider dataStore={new DataStore()}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root')
+  )
 }
 
-const Container = props => (
-  <AppContainer>
-    <Provider dataStore={new DataStore()}>
-      {props.children}
-    </Provider>
-  </AppContainer>
-)
-
-const Routing = () => (
-  <Router>
-    <div>
-      <Route exact path='' component={<Container><App /></Container>} />
-      <Route path='view' component={() => <h1>Hello</h1>} />
-    </div>
-  </Router>
-)
-
-render(Routing)
+render(App)
 
 if (module.hot) {
   module.hot.accept('./App', () => {
-    render(Routing)
+    render(App)
   })
 }
 
