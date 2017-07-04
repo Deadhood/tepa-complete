41b13d569961484351ef13bd5d677da9c233dfb0
Fahad Hossain <evilinfinite7@gmail.com>
2017-07-04 23:35:30 +0600

    tepa

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
+import { withRR4, Nav, NavText } from 'react-sidenav'
+
+import { Route } from 'react-router-dom'
 
 import './App.css'
 
-import NavBar from './NavBar'
 import BalagForm from './BalagForm'
+
+const SideNav = withRR4()
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <div className='col-sm-2'>
-          <NavBar />
+          <div
+            className='navbar-fixed-side'
+            style={{
+              backgroundColor: '#222'
+            }}
+          >
+            <SideNav
+              default='add'
+              highlightBgColor='#111'
+              hoverBgColor='#111'
+              highlightColor='white'
+            >
+              <Nav id='add'>
+                <NavText>Add records</NavText>
+              </Nav>
+            </SideNav>
+          </div>
         </div>
         <div className='col-sm-10 container-fluid'>
           <div className='well'>{this.props.dataStore.message}</div>
-          <BalagForm />
+          <Route exact path='/' component={BalagForm} />
+          <Route path='/add' component={BalagForm} />
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'mobx-react'
 import { AppContainer } from 'react-hot-loader'
+import { HashRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import DataStore from './DataStore'
 import registerServiceWorker from './registerServiceWorker'
 
 import 'bootswatch/cosmo/bootstrap.css'
+import './font.css'
 import './index.css'
+import './navbar-fixed-side.css'
+
+const stores = {
+  dataStore: new DataStore()
+}
 
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Provider dataStore={new DataStore()}>
-        <Component />
+      <Provider {...stores}>
+        <Router>
+          <Component />
+        </Router>
       </Provider>
     </AppContainer>,
     document.getElementById('root')
